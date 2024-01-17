8b94f40bdd066cabe719b640f687edc9327d870f
Andrei Vankevich <156222467+Father1977@users.noreply.github.com>
2024-01-17 22:32:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("""Hello, World
-It is my first python program
-And it is the end my program
-Bye-Bye""")
+number = int(input("Ведите трехзначное число:  "))
+num_1 = number // 100
+num_2 = (number % 100) // 10
+num_3 = number % 10
+print(num_1 + num_2 + num_3)
